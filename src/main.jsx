94c5010eea0e344499e668db2288e3c40d51132b
Mario Ochoa <mario8ato@hotmail.com>
2024-04-13 17:42:48 -0600

    add chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ChakraProvider } from '@chakra-ui/react'
 
 import { global, reset } from "./styles/index.js";
 import { Global } from "@emotion/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ChakraProvider>
     <Global styles={reset} />
     <Global styles={global} />
     <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
